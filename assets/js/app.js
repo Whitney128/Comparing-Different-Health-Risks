642c8cc0b0529239ea201680c5346fe7d744b643
Whitney128 <White.Whitney543@gmail.com>
2020-10-23 20:08:09 -0600

    updated app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,3 @@
-
-
-// var svgWidth = 960;
-// var svgHeight = 500;
-// var margin = {
-//   top: 20,
-//   right: 40,
-//   bottom: 80,
-//   left: 100
-// };
-
-// var width = svgWidth - margin.left - margin.right;
-// var height = svgHeight - margin.top - margin.bottom;
-
-// var svg = d3
-//   .select("#scatter")
-//   .append("svg")
-//   .attr("width", svgWidth)
-//   .attr("height", svgHeight + 40)
-
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 var chosenXAxis = "poverty";
 var chosenYAxis = "healthcare";
 
